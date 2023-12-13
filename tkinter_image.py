e97087051941f370670d897cef495586103f38d7
Marcelo Krochinski <marcelohenriquekrochinski@gmail.com>
2023-12-13 20:56:26 -0300

    add load img e open folder

--- tkinter_image.py
+++ tkinter_image.py
@@ -4,14 +4,70 @@ from tkinter import filedialog
 from PIL import ImageTk, Image, ImageOps
 import os
 
-#define a função do botão open do menu / abrir pasta
-def open_file():
-    folder_path = filedialog.askdirectory()
 
-    if folder_path:
+#variável para armazenar as imagens
+imagens = []
+
+#variável de controle do índice da imagem atual
+imagem_atual = 0
+
+#variavel para armazenar o label da imagem
+img_label = None
+
+#variavel para armazenar o caminho da pasta de imagens
+img_folder = ""
+
+#função para carregar as imagens
+def load_images():
+    global img_folder
+    global imagens
+    global img_label
+
+    #se existe uma pasta de imagens
+    if img_folder:
+        #Limpa a lista de imagens
+        imagens.clear()
+        #lista e procura as imagens da nossa pasta imagens
+        arquivos = os.listdir(img_folder)
+
+
+        #imagens/ é a pasta
+
+        #percorre a lista de arquivos / muda as imagens
+        for arquivo in arquivos:
+            #tratativa de erro
+            try:
+                #abre a imagem
+                img = Image.open(os.path.join(img_folder, arquivo))
+            except:
+                #se não for uma imagem, ignora o arquivo
+                continue
+            else:
+                #redimensiona a imagem / define tamanho padrão para todas as imagens
+                img = ImageOps.contain(img, (200, 200))
+                #adiciona a imagem na lista / transforma o texto(nome do arquivo) em imagem pro Tkinter
+                imagens.append(ImageTk.PhotoImage(img))
+    else:
+        #cria uma imagem em branco
+        img = Image.new("RGB", (200, 200))
+        #adiciona a imagem na lista
+        imagens.append(ImageTk.PhotoImage(img))
+    #carrega a primeira imagem no label
+    img_label = Label(root, image = imagens[0])
+    #exibe a imagem
+    img_label.grid(column=1, row=0, columnspan=3)
+
+
+#define a função do botão open do menu / abrir pasta
+def open_folder():
+    global img_folder
+    img_folder = filedialog.askdirectory()
+
+    if img_folder:
+        load_images()
         messagebox.showinfo(
             title = 'Abrindo diretório...',
-            message=f'O diretório selecionado foi: {folder_path}'
+            message=f'O diretório selecionado foi: {img_folder}'
         )
     else:
         messagebox.showerror(
@@ -30,7 +86,7 @@ menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 
 #adiciona as opções no menu file
-filemenu.add_command(label='Open', command = open_file)
+filemenu.add_command(label='Open', command = open_folder)
 filemenu.add_command(label='Save')
 filemenu.add_command(label='Exit')
 
@@ -40,43 +96,7 @@ menubar.add_cascade(label='File', menu=filemenu)
 #adiciona o menubar na janela
 root.config(menu=menubar)
 
-#lista e procura as imagens da nossa pasta imagens
-arquivos = os.listdir('imagens')
-
-#variável para armazenar as imagens
-imagens = []
-
-#variável de controle do índice da imagem atual
-imagem_atual = 0
-
-
-#imagens/ é a pasta
-
-#percorre a lista de arquivos / muda as imagens
-for arquivo in arquivos:
-    #tratativa de erro
-    try:
-        #abre a imagem
-        img = Image.open('imagens/' + arquivo)
-    except Exception as e:
-        pass
-        print(e)
-    else:
-        #redimensiona a imgem / define tamanho padrão para todas as imagens
-        img = ImageOps.contain(img, (200, 200))
-        #adiciona a imagem na lista / transforma o texto(nome do arquivo) em imagem pro Tkinter
-        imagens.append(ImageTk.PhotoImage(img))
-
-
-
-#Exibe os arquivos em um Label
-#arquivos_label = Label(root, text = arquivos)
-#arquivos_label.pack()
-
-    
-
-img_label = Label(root, image = imagens[imagem_atual])
-img_label.grid(column=1, row=0, columnspan=3)
+load_images()
 
 #defini a função do botão prev
 def prev_image():
